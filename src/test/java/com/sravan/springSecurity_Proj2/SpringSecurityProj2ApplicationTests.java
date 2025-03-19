e14b1c85e951671da757e903aedef8a7912fff44
Sravan Betanapalli(OSV) <sravan.betanapalli.osv@fedex.com>
2025-03-19 12:21:36 +0530

    security jwt proj init

---
+++ src/test/java/com/sravan/springSecurity_Proj2/SpringSecurityProj2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sravan.springSecurity_Proj2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityProj2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
